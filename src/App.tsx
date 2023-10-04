17f75207161bb40a3a34e45dd605212e6a3da6b6
Shangjie Zheng <shangjiez@mail.smu.edu>
2023-10-04 12:10:55 -0500

    modified:   src/App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -12,13 +12,13 @@ const App: React.FC = () => {
   return (
     <Router>
       <Routes>
-        <Route path="/login" element={<LoginPage/>}/>
-        <Route path="/signup" element={<SignUpPage/>}/>
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/signup" element={<SignUpPage />} />
         <Route path="/forgot-password" element={<PasswordResetRequestPage />} />
         <Route path="/password-reset/:token" element={<PasswordResetPage />} />
-        <Route path="/home" element={<Home/>}/>
+        <Route path="/home" element={<Home />} />
         <Route path="/profile" element={<UserProfile />} />
-        <Route path="*" element={<LoginPage/>} />
+        <Route path="*" element={<LoginPage />} />
       </Routes>
     </Router>
   );
