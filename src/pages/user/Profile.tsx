ed785ca5854a170ac8e5111d683548551e967d7d
blake-gebhardt <blakewgebhardt@gmail.com>
2023-10-13 13:25:26 -0500

    building profile page

--- src/pages/user/Profile.tsx
+++ src/pages/user/Profile.tsx
@@ -1,23 +1,12 @@
-import React from 'react';
-import { Container, Typography, Button, Avatar, Box, Input } from '@mui/material';
+import React, { useState } from 'react';
+import { Container, Typography, Button, Avatar, Box, Input, TextField, Paper, Grid } from '@mui/material';
 
 const Profile: React.FC = () => {
-  return (
-    <Container maxWidth="sm">
-      <Box sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center', mt: 8 }}>
-        <Typography component="h1" variant="h5">
-          User Profile
-        </Typography>
-        <Avatar sx={{ width: 56, height: 56, mt: 3 }} alt="User Profile" />
-        <Box sx={{ mt: 2 }}>
-          <Input type="file" id="profileUpload" sx={{ display: 'none' }} onChange={handleFileChange} />
-          <Button variant="contained" color="primary" onClick={handleUploadClick}>
-            Upload Profile
-          </Button>
-        </Box>
-      </Box>
-    </Container>
-  );
+  const [profileImage, setProfileImage] = useState<string | null>(null);
+  const [name, setName] = useState<string>('');
+  const [graduationYear, setGraduationYear] = useState<string>('');
+  const [major, setMajor] = useState<string>('');
+  const [resume, setResume] = useState<string | null>(null);
 
   function handleUploadClick() {
     document.getElementById('profileUpload')?.click();
@@ -26,8 +15,102 @@ const Profile: React.FC = () => {
   function handleFileChange(event: React.ChangeEvent<HTMLInputElement>) {
     const file = event.target.files?.[0];
     if (file) {
-      // Handle the file upload here
+      const reader = new FileReader();
+      reader.onload = (e) => {
+        const uploadedImage = e.target?.result as string;
+        setProfileImage(uploadedImage);
+      };
+      reader.readAsDataURL(file);
     }
+  }
+
+  function handleResumeChange(event: React.ChangeEvent<HTMLInputElement>) {
+    const file = event.target.files?.[0];
+    if (file) {
+      const reader = a FileReader();
+      reader.onload = (e) => {
+        const uploadedResume = e.target?.result as string;
+        setResume(uploadedResume);
+      };
+      reader.readAsDataURL(file);
+    }
+  }
+
+  return (
+    <Container maxWidth="sm">
+      <Box sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center', mt: 8 }}>
+        <Typography component="h1" variant="h5">
+          User Profile
+        </Typography>
+        <Avatar
+          sx={{ width: 100, height: 100, mt: 3 }}
+          alt="User Profile"
+          src={profileImage || undefined}
+        />
+        <Box sx={{ mt: 2 }}>
+          <Grid container spacing={2}>
+            <Grid item xs={6}>
+              <Button variant="contained" color="primary" sx={{ width: '100%' }} onClick={handleUploadClick}>
+                Upload Profile
+              </Button>
+            </Grid>
+            <Grid item xs={6}>
+              <Button variant="contained" color="primary" sx={{ width: '100%' }} onClick={() => document.getElementById('resumeUpload')?.click()}>
+                Upload Resume (PDF)
+              </Button>
+            </Grid>
+          </Grid>
+        </Box>
+        <Box sx={{ mt: 4 }}>
+          <form>
+            <TextField
+              label="Name"
+              variant="outlined"
+              fullWidth
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+            />
+            <TextField
+              label="Graduation Year"
+              variant="outlined"
+              fullWidth
+              value={graduationYear}
+              onChange={(e) => setGraduationYear(e.target.value)}
+            />
+            <TextField
+              label="Major"
+              variant="outlined"
+              fullWidth
+              value={major}
+              onChange={(e) => setMajor(e.target.value)}
+            />
+            <Input type="file" id="resumeUpload" sx={{ display: 'none' }} onChange={handleResumeChange} />
+            {resume && (
+              <a href={resume} target="_blank" rel="noopener noreferrer">
+                View Resume
+              </a>
+            )}
+          </form>
+          <Box sx={{ mt: 2, display: 'flex', justifyContent: 'center' }}>
+            <Button variant="contained" color="primary" sx={{ width: '100%' }} onClick={handleSave}>
+              Save
+            </Button>
+          </Box>
+        </Box>
+        {name && graduationYear && major && (
+          <Paper elevation={3} sx={{ padding: 2, mt: 2, maxWidth: '80%' }}>
+            <Typography variant="h6">User Information</Typography>
+            <Typography>Name: {name}</Typography>
+            <Typography>Graduation Year: {graduationYear}</Typography>
+            <Typography>Major: {major}</Typography>
+          </Paper>
+        )}
+      </Box>
+    </Container>
+  );
+
+  function handleSave() {
+    // Handle saving the user's information
   }
 };
 
