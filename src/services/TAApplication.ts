99d2d91bf8c046b8ec6071d46221af555f1cae16
Miller <Millerjboyd@gmail.com>
2023-10-13 16:06:02 -0500

    frontend api

---
+++ src/services/TAApplication.ts
@@ -0,0 +1,26 @@
+import axios from 'axios';
+import { log } from 'console';
+
+const TA_API_URL = process.env.REACT_APP_BACKEND_URL + '/user/jobs'; 
+
+const getTAApplications = () => {
+  return axios.get(TA_API_URL).then(res => {
+    console.log(res);
+  });
+};
+
+const updateTAApplication = (id: string) => {
+  return axios.put(TA_API_URL + `/${id}`).then( res => {
+    console.log(res);
+  });
+};
+
+// ... [Other potential TA-related API calls]
+
+// Adding TA-related functions to your AuthService export
+const AuthService = {          
+  getTAApplications, 
+  updateTAApplication  
+};
+
+export default AuthService;
