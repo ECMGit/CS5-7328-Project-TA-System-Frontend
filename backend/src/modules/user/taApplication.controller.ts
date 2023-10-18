d393c8c7e7b3097d20cf58625c743e0b8a3fd148
SMJD11 <78318583+SMJD11@users.noreply.github.com>
2023-10-18 16:11:40 -0500

    commennts

--- backend/src/modules/user/taApplication.controller.ts
+++ backend/src/modules/user/taApplication.controller.ts
@@ -11,9 +11,12 @@ import { Request, Response, NextFunction } from 'express';
  * @param res 
  * @param next 
  */
+//this is the controller for the taApplication
 export const getTaApplications = async (req: Request, res: Response, next: NextFunction) => {
     try {
+        // call the service layer function and pass req.query as the parameter
         const app = await UserService.getTaApplications();
+        // send the response
         console.log(app);
         res.json(app);
     } catch (error) {
