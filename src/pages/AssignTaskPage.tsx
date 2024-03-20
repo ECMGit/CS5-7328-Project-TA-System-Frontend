e11665446b9e08326b9cdec3ab0d8a15878468e5
Marcos Zertuche <105753081+Marcos-Zertuche@users.noreply.github.com>
2024-03-20 11:53:04 -0500

    Form finished, need to test and make adjustments on styling and task attributes

--- src/pages/AssignTaskPage.tsx
+++ src/pages/AssignTaskPage.tsx
@@ -5,7 +5,7 @@ import { Link as RouterLink, useNavigate } from 'react-router-dom';
 import { Container, Typography, Avatar, Box, Input, TextField, FormHelperText, Button } from '@mui/material';
 import { LoadingButton } from '@mui/lab';
 
-// import api from '../../services/faculty-job';
+// import api from '../../services/tasks';
 
 
 const AssignTask: React.FC = () => {
@@ -16,6 +16,12 @@ const AssignTask: React.FC = () => {
   const [description, setDescription] = useState('');
   const [urgency, setUrgency] = useState('');
   // TODO: Add Deadline as date value??
+
+  const [message, setMessage] = useState('');
+  const [loading, setLoading] = useState(false);
+
+  //Errors
+  const [Student_idError, setStudent_idError] = useState('');
 
   const checkAlphanumeric = (input: string): boolean => {
     // Check each character of the input
@@ -29,57 +35,71 @@ const AssignTask: React.FC = () => {
     }
     return true; // only alphanumeric characters
   };
+
+  const checkNumeric = (input:string): boolean => {
+    // Check each character of the input
+    for (let i = 0; i < input.length; i++) {
+      const char = input.charCodeAt(i);
+      if (!(char > 47 && char < 58)) return false; // non-numeric character found
+    }
+    return true; // only numeric characters
+  };
+  
+
   //Handler for Title:
 
   //Handler for description:
 
   //Handler for Urgency:
 
+
   //Handler for Student Id:
   const handleStudentIdChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     const input = event.target.value;
-    // IF NUMERIC AND IN DATABASE {
-    //setStudent_id(input)
-    //setStudent_idError('') 
-    //} else {
-    //setStudent_idError('Student id invalid)
+    // IF NUMERIC AND IN DATABASE 
+    if(checkNumeric(input))
+    {
+      setStudent_id(input);
+      setStudent_idError(''); 
+    }
+    else {
+      setStudent_idError('Student id invalid');
+    }
   };
-  
 
 
   const navigate = useNavigate();
 
   const storedUser = localStorage.getItem('user');
 
-
   const handleSubmit = () => {
     const userId = JSON.parse(storedUser!).id; 
-    api.assignTask({
-      facultyId: userId,
-      studentId: student_id,
-      title: title,
-      description: description,
-      urgency: urgency,
-      completed: false,
-      verfied: false
-    }).then(
-      () => {
-        navigate('/tasks');
-        window.location.reload();
-      }, (error) => {
-        const resMessage =
-                    (error.response &&
-                        error.response.data &&
-                        error.response.data.message) ||
-                    error.message ||
-                    error.toString();
+    // api.assignTask({
+    //   facultyId: userId,
+    //   studentId: student_id,
+    //   title: title,
+    //   description: description,
+    //   urgency: urgency,
+    //   completed: false,
+    //   verified: false
+    // }).then(
+    //   () => {
+    //     navigate('/tasks');
+    //     window.location.reload();
+    //   });
+    //   // }, (error) => {
+    //   //   const resMessage =
+    //   //               (error.response &&
+    //   //                   error.response.data &&
+    //   //                   error.response.data.message) ||
+    //   //               error.message ||
+    //   //               error.toString();
 
-        setLoading(false);
-        setMessage(resMessage);
-      });
+    //   //   setLoading(false);
+    //   //   setMessage(resMessage);
+    //   // });
   };
-
-  // JSX for rendering the form
+    // JSX for rendering the form
   return (
     <Container maxWidth="sm">
       <Box sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center', mt: 8, }} >
@@ -87,7 +107,16 @@ const AssignTask: React.FC = () => {
                     Assign TA Task
         </Typography>
         <Box component="form" onSubmit={handleSubmit} mt={3}>
-          <TextField label="Title" margin="normal" required fullWidth autoComplete="name" onChange={(e) => { setTitle(e.target.value); }} autoFocus />
+          {/* Title */}
+          <TextField 
+            label="Title " 
+            margin="normal" 
+            required 
+            fullWidth 
+            autoComplete="name" 
+            onChange={(e) => { setTitle(e.target.value); }} 
+            autoFocus />
+          {/* Student Task Assigned to */}
           <TextField
             label="Assignee (Student ID)"
             margin="normal"
@@ -95,57 +124,53 @@ const AssignTask: React.FC = () => {
             fullWidth
             autoComplete="off"
             value={student_id}
-            // onChange={handleCourseIdChange}
-            // error={!!courseIdError}
-            // helperText={courseIdError}
+            onChange={handleStudentIdChange}
+            error={!!Student_idError}
+            helperText={Student_idError}
             autoFocus
           />
-          <TextField label="Course Schedule" margin="normal" required fullWidth onChange={(e) => { setCourseSchedule(e.target.value); }} />
+          {/* Task Description */}
+          <TextField 
+            label="Description" 
+            margin="normal" 
+            required 
+            fullWidth 
+            onChange={(e) => { setDescription(e.target.value); }} 
+          />
+          {/* Task Urgency 
+          May convert to High , Medium, Low*/}
           <TextField
-            label="Total Hour"
+            label="Urgency (Number 1-5  "
             margin="normal"
             required
             fullWidth
             type="number"
             inputProps={{ min: 0 }}
-            error={!totalHour || isNaN(Number(totalHour))}
-            helperText={!totalHour || isNaN(Number(totalHour)) ? 'Total Hour must be a number' : ''}
-            onChange={(e) => { setTotalHour(e.target.value); }}
+            error={!urgency || isNaN(Number(urgency))}
+            helperText={!urgency || isNaN(Number(urgency)) ? 'Urgency must be a number' : ''}
+            onChange={(e) => { setUrgency(e.target.value); }}
           />
-          <TextField
-            label="Max TA Count"
-            margin="normal"
-            required
-            fullWidth
-            type="number"
-            inputProps={{ min: 0 }}
-            error={!maxTaCount || isNaN(Number(maxTaCount))}
-            helperText={!maxTaCount || isNaN(Number(maxTaCount)) ? 'Max TA Count must be a number' : ''}
-            onChange={(e) => { setMaxTaCount(e.target.value); }}
-          />
-          <TextField
-            label="Required Course"
-            margin="normal"
-            required
-            fullWidth
-            autoComplete="off"
-            value={requiredCourses}
-            onChange={handleRequiredCoursesChange}
-            error={!!requiredCoursesError}
-            helperText={requiredCoursesError}
-          />
-          <TextField label="Required Skills" margin="normal" required fullWidth onChange={(e) => { setRequiredSkills(e.target.value); }} />
-          <TextField label="TA Stats" margin="normal" required fullWidth onChange={(e) => { setTaStats(e.target.value); }} />
-          <TextField label="Notes" margin="normal" fullWidth onChange={(e) => { setNotes(e.target.value); }} />
-          <TextField label="Deadline" margin="normal" required fullWidth onChange={(e) => { setDeadline(e.target.value); }} />
-          <LoadingButton type="submit" variant="contained" loading={loading} sx={{ mt: 4, mb: 3 }}>Post Job</LoadingButton>
-          <Button component={RouterLink} variant="text" to='/jobs' sx={{ mt: 4, mb: 3 }} >Cancel</Button>
+          <LoadingButton 
+            type="submit" 
+            variant="contained" 
+            loading={loading} 
+            sx={{ mt: 4, mb: 3 }}
+          >
+              Post Job
+          </LoadingButton>
+          <Button 
+            component={RouterLink} 
+            variant="text" 
+            to='/tasks' 
+            sx={{ mt: 4, mb: 3 }} 
+          >
+            Cancel
+          </Button>
           <FormHelperText>{message}</FormHelperText>
         </Box>
       </Box>
     </Container>
   );
-
 };
 
-export default PostJob;
+export default AssignTask;
