4b2c6ce5dd337a4c4355c5b30e6788594eaba236
XiaofengChen0821 <60332172+XiaofengChen0821@users.noreply.github.com>
2024-04-23 15:33:16 -0500

    Top Nav Bar for Faculty and Student Profile page updated. (#2)

--- src/pages/user/FacultyProfile.tsx
+++ src/pages/user/FacultyProfile.tsx
@@ -39,9 +39,6 @@ interface Job {
   facultyId: number;
 }
 
-
-
-
 const FacultyProfile: React.FC = () => {
   const [profileImage, setProfileImage] = useState<string | null>(null);
   const [name, setName] = useState<string>('');
@@ -92,7 +89,6 @@ const FacultyProfile: React.FC = () => {
   const [anchorEl, setAnchorEl] = useState<null | Element>(null);
 
   const open = Boolean(anchorEl);
-
 
   //TODO: Remove test message
   const [messages, setMessages] = useState([
@@ -153,259 +149,202 @@ const FacultyProfile: React.FC = () => {
     handleClose();
   };
 
-
   /* Note - The inbox menu and the profile menu don't make sense being together. 
   A solution -> put view messages button in profile menu*/
   return (
-    <Container>
-      <Box
-        sx={{
-          display: 'flex',
-          justifyContent: 'space-between',
-          alignItems: 'center',
-          backgroundColor: '#1976D2', // Blue color
-          color: '#FFF', // White color
-          padding: '16px', // Adjust the padding as needed
-        }}
-      >
-        <TopNav />
-        {/* Old menu 
-        <Tooltip title="Menu">
-          <IconButton
-            color="inherit"
-            aria-controls="simple-menu"
-            aria-haspopup="true"
-            onClick={handleClick}
-          >
-            <MenuIcon />
-          </IconButton>
-        </Tooltip>
-        
-        <Menu
-          id="simple-menu"
-          anchorEl={anchorEl}
-          keepMounted
-          open={open}
-          onClose={handleClose}
-        >
-          <MenuItem onClick={navigateToInbox}>
-            {/* <ListItemIcon>
-              <MailIcon fontSize="small" />
-            </ListItemIcon>
-            Inbox
-            <Tooltip title="Inbox">
-              <IconButton
-                color="inherit"
-                onClick={() => {
-                  // Fetch messages when the inbox is opened (future implementation)
-                  fetchMessages();
-                }}
-              >
-                <MailIcon />
-              </IconButton>
-            </Tooltip>
-            {/* Add an inbox UI element
-            {/* <Menu
-              id="inbox-menu"
-              anchorEl={anchorEl}
-              keepMounted
-              open={open}
-              onClose={handleClose}
+    <>
+      <TopNav />
+      <Container>
+        <Grid container spacing={4}>
+          <Grid item xs={6}>
+            <Box
+              sx={{
+                display: 'flex',
+                flexDirection: 'column',
+                alignItems: 'center',
+              }}
             >
-              {messages.map((message) => (
-                <MenuItem key={message.id}>{message.content}</MenuItem>
-              ))}
-            </Menu>
-          </MenuItem>
-        </Menu>*/}
-      </Box>
-      <Grid container spacing={4}>
-        <Grid item xs={6}>
-          <Box
-            sx={{
-              display: 'flex',
-              flexDirection: 'column',
-              alignItems: 'center',
-            }}
-          >
-            <Typography component="h1" variant="h5">
-              Faculty Profile
-            </Typography>
-            <Avatar
-              sx={{ width: 200, height: 200, mt: 3 }}
-              alt="User Profile"
-              src={profileImage || undefined}
-            />
-            <Typography>Name: {name}</Typography>
-            <Typography>Department: {department}</Typography>
-            <Box sx={{ mt: 2 }}>
-              <Grid container spacing={2}>
-                <Grid item xs={6}>
-                  <Button
-                    variant="contained"
-                    color="primary"
-                    sx={{ width: '100%' }}
-                    onClick={handleUploadClick}
-                  >
-                    Upload Profile
-                  </Button>
+              <Typography component="h1" variant="h5">
+                Faculty Profile
+              </Typography>
+              <Avatar
+                sx={{ width: 200, height: 200, mt: 3 }}
+                alt="User Profile"
+                src={profileImage || undefined}
+              />
+              <Typography>Name: {name}</Typography>
+              <Typography>Department: {department}</Typography>
+              <Box sx={{ mt: 2 }}>
+                <Grid container spacing={2}>
+                  <Grid item xs={6}>
+                    <Button
+                      variant="contained"
+                      color="primary"
+                      sx={{ width: '100%' }}
+                      onClick={handleUploadClick}
+                    >
+                      Upload Profile
+                    </Button>
+                    <Input
+                      type="file"
+                      id="profileUpload"
+                      sx={{ display: 'none' }}
+                      onChange={handleFileChange}
+                    />
+                  </Grid>
+                  <Grid item xs={6}>
+                    <Button
+                      variant="contained"
+                      color="primary"
+                      sx={{ width: '100%' }}
+                      onClick={() =>
+                        document.getElementById('resumeUpload')?.click()
+                      }
+                    >
+                      Upload Resume
+                    </Button>
+                  </Grid>
+                </Grid>
+              </Box>
+              <Box sx={{ mt: 4 }}>
+                <form>
+                  {/* <TextField
+                    label="Name"
+                    variant="outlined"
+                    fullWidth
+                    value={name}
+                    onChange={(e) => setName(e.target.value)}
+                    sx={{ mb: 2 }}
+                  />
+                  <TextField
+                    label="Department"
+                    variant="outlined"
+                    fullWidth
+                    value={department}
+                    onChange={(e) => setDepartment(e.target.value)}
+                    sx={{ mb: 2 }}
+                  /> */}
                   <Input
                     type="file"
-                    id="profileUpload"
+                    id="resumeUpload"
                     sx={{ display: 'none' }}
-                    onChange={handleFileChange}
+                    onChange={handleResumeChange}
                   />
-                </Grid>
-                <Grid item xs={6}>
+                  {resume && (
+                    <a href={resume} target="_blank" rel="noopener noreferrer">
+                      View Resume
+                    </a>
+                  )}
+                </form>
+                <Box
+                  sx={{ mt: 2, display: 'column', justifyContent: 'center' }}
+                >
                   <Button
                     variant="contained"
                     color="primary"
                     sx={{ width: '100%' }}
-                    onClick={() =>
-                      document.getElementById('resumeUpload')?.click()
-                    }
+                    onClick={handleSave}
                   >
-                    Upload Resume
+                    Save
                   </Button>
-                </Grid>
-              </Grid>
-            </Box>
-            <Box sx={{ mt: 4 }}>
-              <form>
-                {/* <TextField
-                  label="Name"
-                  variant="outlined"
-                  fullWidth
-                  value={name}
-                  onChange={(e) => setName(e.target.value)}
-                  sx={{ mb: 2 }}
-                />
-                <TextField
-                  label="Department"
-                  variant="outlined"
-                  fullWidth
-                  value={department}
-                  onChange={(e) => setDepartment(e.target.value)}
-                  sx={{ mb: 2 }}
-                /> */}
-                <Input
-                  type="file"
-                  id="resumeUpload"
-                  sx={{ display: 'none' }}
-                  onChange={handleResumeChange}
-                />
-                {resume && (
-                  <a href={resume} target="_blank" rel="noopener noreferrer">
-                    View Resume
-                  </a>
-                )}
-              </form>
-              <Box sx={{ mt: 2, display: 'column', justifyContent: 'center' }}>
-                <Button
-                  variant="contained"
-                  color="primary"
-                  sx={{ width: '100%' }}
-                  onClick={handleSave}
-                >
-                  Save
-                </Button>
-                <Button
-                  component={Link}
-                  to="/jobs"
-                  variant="contained"
-                  color="primary"
-                  style={{
-                    marginTop: '8px',
-                    width: '100%',
-                    textAlign: 'center',
-                  }}
-                >
-                  View All Jobs
-                </Button>
+                  <Button
+                    component={Link}
+                    to="/jobs"
+                    variant="contained"
+                    color="primary"
+                    style={{
+                      marginTop: '8px',
+                      width: '100%',
+                      textAlign: 'center',
+                    }}
+                  >
+                    View All Jobs
+                  </Button>
+                </Box>
               </Box>
+              {/* {name && department && (
+                <Paper elevation={3} sx={{ padding: 2, mt: 2, maxWidth: '80%' }}>
+                  <Typography variant="h6">User Information</Typography>
+                  <Typography>Name: {name}</Typography>
+                  <Typography>Department: {department}</Typography>
+                </Paper>
+              )} */}
             </Box>
-            {/* {name && department && (
-              <Paper elevation={3} sx={{ padding: 2, mt: 2, maxWidth: '80%' }}>
-                <Typography variant="h6">User Information</Typography>
-                <Typography>Name: {name}</Typography>
-                <Typography>Department: {department}</Typography>
-              </Paper>
-            )} */}
-          </Box>
+          </Grid>
+          <Grid item xs={6}>
+            {/* Right section with Job Boxes using Box components */}
+            {/* These boxes should be active applications or open positions that you've filled*/}
+            <Box
+              sx={{
+                mt: '50px',
+                display: 'flex',
+                flexDirection: 'column',
+                alignItems: 'center',
+              }}
+            >
+              {jobs.map((job) => (
+                <Paper
+                  key={job.id}
+                  elevation={3}
+                  sx={{ spacing: 2, padding: 2, mb: 2, width: '100%' }}
+                >
+                  <Typography variant="h6">{job.title}</Typography>
+                  <Typography>{job.notes}</Typography>
+                  <Typography>Date Submitted: {job.deadlineToApply}</Typography>
+                  <Button
+                    variant="contained"
+                    color="primary"
+                    style={{ marginRight: '10px' }}
+                    onClick={() => handleCheckApplicants(job.id)}
+                  >
+                    Check Applicants
+                  </Button>
+                  <Button
+                    variant="contained"
+                    color="secondary"
+                    onClick={() => handleEditPosting(job.id)}
+                  >
+                    Edit Posting
+                  </Button>
+                </Paper>
+              ))}
+            </Box>
+            {/* Box for current TA and evaluate performance */}
+            <Box
+              sx={{
+                mt: '50px',
+                display: 'flex',
+                flexDirection: 'column',
+                alignItems: 'center',
+              }}
+            >
+              {currentTAs.map((ta) => (
+                <Paper
+                  key={ta.courseId}
+                  elevation={3}
+                  sx={{ spacing: 2, padding: 2, mb: 2, width: '100%' }}
+                >
+                  <Typography variant="h6">TA Name: {ta.username}</Typography>
+                  <Typography>SMU ID: {ta.smuNo}</Typography>
+                  <Typography>Course ID: {ta.courseId}</Typography>
+                  <Typography>course Code: {ta.courseCode}</Typography>
+                  <Typography>Course Name: {ta.title}</Typography>
+                  {/* TA performance evaluation button */}
+                  <Button
+                    onClick={() => handleEvaluateTA(ta)}
+                    variant="contained"
+                    color="primary"
+                    sx={{ mt: 2, alignSelf: 'center' }}
+                  >
+                    Evaluate TA
+                  </Button>
+                </Paper>
+              ))}
+            </Box>
+          </Grid>
         </Grid>
-        <Grid item xs={6}>
-          {/* Right section with Job Boxes using Box components */}
-          {/* These boxes should be active applications or open positions that you've filled*/}
-          <Box
-            sx={{
-              mt: '50px',
-              display: 'flex',
-              flexDirection: 'column',
-              alignItems: 'center',
-            }}
-          >
-            {jobs.map((job) => (
-              <Paper
-                key={job.id}
-                elevation={3}
-                sx={{ spacing: 2, padding: 2, mb: 2, width: '100%' }}
-              >
-                <Typography variant="h6">{job.title}</Typography>
-                <Typography>{job.notes}</Typography>
-                <Typography>Date Submitted: {job.deadlineToApply}</Typography>
-                <Button
-                  variant="contained"
-                  color="primary"
-                  style={{ marginRight: '10px' }}
-                  onClick={() => handleCheckApplicants(job.id)}
-                >
-                  Check Applicants
-                </Button>
-                <Button
-                  variant="contained"
-                  color="secondary"
-                  onClick={() => handleEditPosting(job.id)}
-                >
-                  Edit Posting
-                </Button>
-              </Paper>
-            ))}
-          </Box>
-          {/* Box for current TA and evaluate performance */}
-          <Box
-            sx={{
-              mt: '50px',
-              display: 'flex',
-              flexDirection: 'column',
-              alignItems: 'center',
-            }}
-          >
-            {currentTAs.map((ta) => (
-              <Paper
-                key={ta.courseId}
-                elevation={3}
-                sx={{ spacing: 2, padding: 2, mb: 2, width: '100%' }}
-              >
-                <Typography variant="h6">TA Name: {ta.username}</Typography>
-                <Typography>SMU ID: {ta.smuNo}</Typography>
-                <Typography>Course ID: {ta.courseId}</Typography>
-                <Typography>course Code: {ta.courseCode}</Typography>
-                <Typography>Course Name: {ta.title}</Typography>
-                {/* TA performance evaluation button */}
-                <Button
-                  onClick={() => handleEvaluateTA(ta)}
-                  variant="contained"
-                  color="primary"
-                  sx={{ mt: 2, alignSelf: 'center' }}
-                >
-                  Evaluate TA
-                </Button>
-              </Paper>
-            ))}
-          </Box>
-        </Grid>
-      </Grid>
-    </Container>
+      </Container>
+    </>
   );
 
   function handleSave() {
@@ -415,7 +354,7 @@ const FacultyProfile: React.FC = () => {
   function handleCheckApplicants(jobId: number) {
     // Handle checking applicants for the specified job ID
     //TODO: Implement this function and pull data from backend -- team3 sprint3
-    
+
     console.log(`Checking applicants for job ${jobId}`);
     navigate('/view-applications');
   }
@@ -423,7 +362,7 @@ const FacultyProfile: React.FC = () => {
   function handleEditPosting(jobId: number) {
     // Handle editing the posting for the specified job ID
     //TODO: Implement this function and pull data from backend -- team3 sprint3
-    
+
     console.log(`Editing posting for job ${jobId}`);
     navigate(`/edit-job/${jobId}`);
   }
