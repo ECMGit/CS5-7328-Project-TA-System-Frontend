2c62802a1f69f49cea45ad93748d6b1f8768b8a1
XiaofengChen <60332172+XiaofengChen0821@users.noreply.github.com>
2024-04-30 15:33:31 -0500

    Applications status for ViewApplicationsByJobID page updated with highlight.

--- src/pages/application/ViewApplicationsByJobID.tsx
+++ src/pages/application/ViewApplicationsByJobID.tsx
@@ -8,6 +8,7 @@ import {
   ListItem,
   ListItemText,
   Paper,
+  Alert,
 } from '@mui/material';
 import TopNav from '../../components/TopNav';
 
@@ -28,65 +29,100 @@ interface TAApplicationData {
 
 const ViewApplicationsByJobID: React.FC = () => {
   const location = useLocation();
-  const { taApplications } = location.state as { taApplications: TAApplicationData[] };
+  const { taApplications } = location.state as {
+    taApplications: TAApplicationData[];
+  };
 
   return (
     <div>
-    <TopNav />
-    <Container component="main" maxWidth="md">
-      <Box sx={{ marginTop: 4 }}>
-        <Typography variant="h5" gutterBottom>
-          TA Applications for Job ID: {taApplications[0]?.taJobId}
-        </Typography>
-        {taApplications.length > 0 ? (
-          <List>
-            {taApplications.map((app, index) => (
-              <Paper key={index} elevation={2} sx={{ margin: '20px', padding: '20px' }}>
-                <ListItem sx={{ border: '1px solid #ccc', borderRadius: '4px', padding: '10px' }}>
-                  <ListItemText
-                    primary={`Application ID: ${app.id} - Course ID: ${app.courseId}`}
-                    secondary={
-                      <>
-                        <Typography component="span" variant="body2">
-                          Student ID: {app.studentId} - GPA: {app.gpa}
-                        </Typography>
-                        <br />
-                        <Typography component="span" variant="body2">
-                          Hours Can Work Per Week: {app.hoursCanWorkPerWeek}
-                        </Typography>
-                        <br />
-                        <Typography component="span" variant="body2">
-                          Courses Taken: {app.coursesTaken}
-                        </Typography>
-                        <br />
-                        <Typography component="span" variant="body2">
-                          Required Courses: {app.requiredCourses}
-                        </Typography>
-                        <br />
-                        <Typography component="span" variant="body2">
-                          Required Skills: {app.requiredSkills}
-                        </Typography>
-                        <br />
-                        <Typography component="span" variant="body2">
-                          Resume: <a href={app.resumeFile} target="_blank" rel="noopener noreferrer">View Resume</a>
-                        </Typography>
-                        <br />
-                        <Typography component="span" variant="body2">
-                          Status: {app.status}
-                        </Typography>
-                      </>
-                    }
-                  />
-                </ListItem>
-              </Paper>
-            ))}
-          </List>
-        ) : (
-          <Typography>No TA applications found for job ID.</Typography>
-        )}
-      </Box>
-    
-    </Container>
+      <TopNav />
+      <Container component="main" maxWidth="md">
+        <Box sx={{ marginTop: 4 }}>
+          <Typography variant="h5" gutterBottom>
+            TA Applications for Job ID: {taApplications[0]?.taJobId}
+          </Typography>
+          {taApplications.length > 0 ? (
+            <List>
+              {taApplications.map((app, index) => (
+                <Paper
+                  key={index}
+                  elevation={2}
+                  sx={{ margin: '20px', padding: '20px' }}
+                >
+                  <ListItem
+                    sx={{
+                      border: '1px solid #ccc',
+                      borderRadius: '4px',
+                      padding: '10px',
+                    }}
+                  >
+                    <ListItemText
+                      primary={`Application ID: ${app.id} - Course ID: ${app.courseId}`}
+                      secondary={
+                        <>
+                          <Typography component="span" variant="body2">
+                            Student ID: {app.studentId} - GPA: {app.gpa}
+                          </Typography>
+                          <br />
+                          <Typography component="span" variant="body2">
+                            Hours Can Work Per Week: {app.hoursCanWorkPerWeek}
+                          </Typography>
+                          <br />
+                          <Typography component="span" variant="body2">
+                            Courses Taken: {app.coursesTaken}
+                          </Typography>
+                          <br />
+                          <Typography component="span" variant="body2">
+                            Required Courses: {app.requiredCourses}
+                          </Typography>
+                          <br />
+                          <Typography component="span" variant="body2">
+                            Required Skills: {app.requiredSkills}
+                          </Typography>
+                          <br />
+                          <Typography component="span" variant="body2">
+                            Resume:{' '}
+                            <a
+                              href={app.resumeFile}
+                              target="_blank"
+                              rel="noopener noreferrer"
+                            >
+                              View Resume
+                            </a>
+                          </Typography>
+                          <br />
+                          <Typography component="span" variant="body2">
+                            {/* Status: {app.status} */}
+                            {app.status === 'Pending' ? (
+                              <Alert severity="warning">
+                                Application Status: {app.status}
+                              </Alert>
+                            ) : app.status === 'Approved' ? (
+                              <Alert severity="success">
+                                Application Status: {app.status}
+                              </Alert>
+                            ) : app.status === 'Rejected' ? (
+                              <Alert severity="error">
+                                Application Status: {app.status}
+                              </Alert>
+                            ) : (
+                              <Alert severity="info">
+                                Application Status: {app.status}
+                              </Alert>
+                            )}
+                          </Typography>
+                        </>
+                      }
+                    />
+                  </ListItem>
+                </Paper>
+              ))}
+            </List>
+          ) : (
+            <Typography>No TA applications found for job ID.</Typography>
+          )}
+        </Box>
+      </Container>
     </div>
   );
 };
