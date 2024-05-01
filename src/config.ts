a37f8bd1fc29d031fafae76d72314dc06a65f7a4
Ethan Zech <ezech@smu.edu>
2024-05-01 18:45:51 -0500

    Changed timeout timers and added note

--- src/config.ts
+++ src/config.ts
@@ -3,6 +3,6 @@ const backendURL =
     ? 'https://your-production-backend-url.com'
     : 'http://localhost:9000';
 
-const timeoutWaitTime = 10000;
-const timeoutDialogTime = 20000;
+const timeoutWaitTime = 600000; //Time in milliseconds before inactivity prompt appears
+const timeoutDialogTime = 30000; //Time in milliseconds before inactivity prompt kicks user to log in
 export { backendURL, timeoutWaitTime, timeoutDialogTime };
